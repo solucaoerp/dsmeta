6835a2d9693d09f975e48bcd49cd35960fa79057
Charles Borges <solucao.erp@gmail.com>
2022-07-12 23:06:16 -0300

    First component: NotificationButton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
-export default function App() {
+import NotificationButton from "./components/NotificationButton";
+
+function App() {
   return (
-    <h1>iniciando...</h1>
+    <>
+      <h1>iniciando...</h1>
+      <NotificationButton />
+    </>
   )
 }
+
+export default App
