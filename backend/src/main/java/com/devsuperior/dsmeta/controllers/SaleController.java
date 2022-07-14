1db9f3ec2794513ca2bac11e6caf2263bd1f6b65
Charles Borges <solucao.erp@gmail.com>
2022-07-14 14:57:45 -0300

    backend: select by date with refactoring and default values in request

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,8 +3,11 @@ package com.devsuperior.dsmeta.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -18,8 +21,21 @@ public class SaleController {
 	private SaleService service;
 
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public List<Sale> findAllSales() {
+		return service.findAllSales();
 	}
+	
+	@GetMapping(value = "/list-by-page")
+	public Page<Sale> findAllSalesByPage(Pageable pageable) {
+		return service.findAllSalesByPage(pageable);
+	}
+	
+	@GetMapping(value = "/list-by-date")
+	public Page<Sale> findAllSalesByDate(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
 
+		return service.findAllSalesByDate(minDate, maxDate, pageable);
+	}
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -14,8 +19,22 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 
-	public List<Sale> findSales() {
+	public List<Sale> findAllSales() {
 		return repository.findAll();
 	}
+	
+	public Page<Sale> findAllSalesByPage(Pageable pageable) {
+		return repository.findAll(pageable);
+	}
+	
+	public Page<Sale> findAllSalesByDate(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()); // create date with today
 
+		// Date in String format to LocalDate - converter - to customize JPQL expression in repository
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate); // ternary conditional condition
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);                // ternary conditional condition
+
+		return repository.findAllSalesByDate(min, max, pageable);
+	}	
 }
