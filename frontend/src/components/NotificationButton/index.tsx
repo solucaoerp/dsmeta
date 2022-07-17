acc8c1f3db0f856bb0195176dccaa33a84c3408b
Charles Borges <solucao.erp@gmail.com>
2022-07-17 15:24:59 -0300

    frontend: props - send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("SUCESSO");
+        })
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="dsmeta_red_btn">
+        <div className="dsmeta_red_btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
